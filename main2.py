7c233e64e2c703a62300eb4f376fabef083d0fbe
guillaumelevesque <guil.lvsq@gmail.com>
2020-10-15 17:36:51 -0400

    rgb2yuv

--- main2.py
+++ main2.py
@@ -9,34 +9,19 @@ def rgb2yuv(rgb):
             r = pixel[0]
             g = pixel[1]
             b = pixel[2]
-            yuv[i][j][0] = (np.float64((r+2*g+b)/4)/255)
-            yuv[i][j][1] = (np.float64((r-g)/255))
-            yuv[i][j][2] = (np.float64((b-g)/255))
-    print("yuv", yuv)
+            yuv[i][j][0] = np.clip(np.float64((r+2*g+b)/4), 0, 255)
+            yuv[i][j][1] = np.clip(np.float64((b-g)), 0, 255)
+            yuv[i][j][2] = np.clip(np.float64((r-g)), 0, 255)
     return yuv
 
 
 imagelue = py.imread('cageSmall.jpeg')
-print(type(imagelue[0][0][0]))
-print("lue", imagelue)
-image = imagelue.astype(np.float)
-print(type(image[0][0][0]))
-print("asFloat", image)
+print("rgb", imagelue[110][110])
 
-imageYUV = rgb2yuv(image)
-
-imageout = image.astype(np.uint8)
-print(type(imageYUV[0][0][0]))
-print("asUint8", imageout)
-
-py.imshow(imageout)
-py.show()
-
-# imageYUV = imageYUV.astype(np.uint8)
-print(type(imageYUV[0][0][0]))
-print("asUint8", imageout)
-py.imshow(imageYUV)
-py.show()
+imageYUV = rgb2yuv(imagelue.astype(np.float)).astype(np.uint8)
+print("yuv", imageYUV[110][110])
+# py.imshow(imageYUV)
+# py.show()
 
 
 
